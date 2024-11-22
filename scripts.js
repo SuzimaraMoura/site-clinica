bdc355b3e0fc5d30bce2194fd60f0268b245d39e
SuzimaraMoura <suzi.moura.ramalho@gmail.com>
2024-11-22 16:46:27 -0300

    Projeto Clínica da Mulher

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocarImagem() {
+    const imagem = document.getElementById("doutora");
+    imagem.src = "img/doutora2.webp"; // Caminho da nova imagem
+  }
+  
+  function restaurarImagem() {
+    const imagem = document.getElementById("doutora");
+    imagem.src = "img/doutora3.webp"; // Caminho da imagem original
+  }
